ee065f7b04b6d85cb17f89e5c2446cbcc401cd49
KossySteve <kossyeze@gmail.com>
2020-04-23 01:33:18 +0100

    effected change from codereview

--- bin/main.rb
+++ bin/main.rb
@@ -18,22 +18,27 @@ puts '___1_|__2__|__3__'
 puts '___4_|__5__|__6__'
 puts '___7_|__8__|__9__'
 
-3.times do
+# Start playing
+game_on = true
+while game_on
   puts "player1 choose any of the numbered positions from 1 to 9 that hasn't been played on to play #{player_1_symbol}"
   gets.chomp
   # Player 1 will play
-  puts 'X' # at chosen position
+  puts 'X or O' # at chosen position
   puts "player2 choose any of the numbered positions from 1 to 9 that hasn't been played on to play #{player_2_symbol}"
   gets.chomp
   # Player 2 will play
-  puts 'O' # at chosen position
+  puts 'O or X' # at chosen position
   puts "player1 choose any of the numbered positions from 1 to 9 to play #{player_1_symbol}"
   gets.chomp
   # Player 1 will play
-  puts 'X' # at chosen position
-end
+  puts 'X or O' # at chosen position
 
-# Draw
-puts 'Its a draw~! or player1 wins! or player2 wins!'
-puts 'Game Over'
+  if winner || draw
+    puts 'Its a draw~! or player1 wins! or player2 wins!'
+    game_on = false
+    puts 'Game Over'
+  end
+end
+# To start game again
 puts 'Do you want to play again?'
