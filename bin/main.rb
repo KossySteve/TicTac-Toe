cf2a135f1869e22ed6a36475480f54d595f59065
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-04-26 18:29:43 +0300

    fix turn method

--- bin/main.rb
+++ bin/main.rb
@@ -11,40 +11,42 @@ class Game
     puts 'Welcome to Tic Tac Toe'
     puts '----------+-----------'
     puts 'player 1 what is your name?'
-    player1 = gets.chomp
+    player_one = gets.chomp
 
-    puts " #{player1} choose your symbol either X or O to play "
+    puts " #{player_one} choose your symbol either X or O to play "
     symbol = gets.chomp
     unless $game_symbols.include? symbol
       puts 'Only enter X or O'
-      puts " #{player1} choose your symbol either X or O to play "
+      puts " #{player_one} choose your symbol either X or O to play "
       symbol = gets.chomp.upcase
     end
 
     $game_symbols.delete(symbol)
     puts 'player 2 what is your name?'
-    player2 = gets.chomp
-    symbol2 = $game_symbols[0]
-    puts "#{player2} your symbol is #{symbol2}"
+    player_two = gets.chomp
+    symbol_two = $game_symbols[0]
+    puts "#{player_two} your symbol is #{symbol_two}"
     puts ''
 
-    @player_1 = Player.new(player1, symbol)
-    @player_2 = Player.new(player2, symbol2)
+    @player_1 = Player.new(player_one, symbol)
+    @player_2 = Player.new(player_two, symbol_two)
     @board = Board.new
     @board.show
   end
 
   def turn(player)
-    puts "#{player.name} pick a number position on the board you want to play on"
+    puts "#{player.name} pick a box to mark"
     pick = gets.chomp
-    cell = pick.to_i - 1
-    if @board.field[cell] == 'X' or @board.field[cell] == 'O' or pick.length != 1 or !pick == (1..9)
-      puts 'invalid choice... pls choose positions not filled from 1 to 9'
+    cell = pick.to_i-1
+    #check if the cell is occupied 
+    if @board.field[cell] == "X" || @board.field[cell] == "O" || pick.length != 1 || !pick[/[1-9]/]
+        puts "Invalid Cell"
+        self.turn(player)
     else
-      @board.field[cell] = player.symbol
-      @board.show
+        @board.field[cell] =player.symbol
+        @board.show
     end
-  end
+end
 
   def game_over
     case @board.winner
@@ -87,7 +89,7 @@ class Game
     game_over
   end
 end
-Game.new.play_again
+Game.new.play
 
 # rubocop:enable Style/GlobalVars
 # rubocop:enable Metrics/MethodLength
