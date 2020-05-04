d6c7f13d588d87b659ff4892e0ce6912a9454916
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-05-04 23:10:26 +0300

    test code

---
+++ spec/game_spec.rb
@@ -0,0 +1,16 @@
+
+require './lib/board.rb'
+
+
+RSpec.describe do
+  describe 'Game' do
+    let (:board) { Board.new }
+    it 'shows board' do
+      
+      expect(board.show.class).to eq(
+        
+      )
+    end
+  end
+  
+end
