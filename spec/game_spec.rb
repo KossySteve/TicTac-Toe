fcff21dc090414f3f16a508dae548380bd37e1d6
KossySteve <kossyeze@gmail.com>
2020-05-06 03:46:25 +0100

    added test for more Main methods

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,41 +4,49 @@ require './lib/game.rb'
 require './bin/main.rb'
 
 describe Board do
+  game_board = Board.new
   describe "#winner" do
     it "returns false when there is no winner" do
-      a_winner = Board.new
-      expect(a_winner.winner).to eql(false)
+      expect(game_board.winner).to eql(false)
     end
-    it "returns the game board" do
-      game_board = Board.new
-      game_board.show
+    it "prints the game board" do
+      expect(game_board.show).to be_nil
     end
     it "returns 9 cells to play on" do
-      game_board = Board.new
       expect(game_board.field.length).to eql(9)
     end
     it "board is empty or is not full as game begins" do
-      game_board = Board.new
       expect(game_board.full?).to eql(false)
     end
   end
 end
 
 describe Game do
+  game = Game.new
+  board = Board.new
   describe "#play" do
     it "instance of Game calls play method" do
-      game = Game.new
       expect(game).to receive(:play)
       game.play
     end
     it "return game_over when there's a winner " do
-      game = Game.new
-      board = Board.new
       expect(game.play).to eql(:game_over) if board.winner == 'X'
     end
     it "responds to Main methods" do
-      game = Game.new
       expect(game).to respond_to(:turn, :play_again, :start, :game_over)
+    end
+  end
+end
+
+describe Main do
+  describe "#start" do
+    it "create and stores player name" do
+      @player_one = Player.new("Mark")
+       expect(@player_one.name).to eq("Mark")
+    end
+    it "creates and stores player symbol" do
+      @player_one = Player.new("Jana", "X")
+       expect(@player_one.symbol).to eq("X")
     end
   end
 end
